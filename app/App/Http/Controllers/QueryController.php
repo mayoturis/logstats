4a4343fad60853578b9ca17567cc3942943ebb78
mayo505 <marek.turis@gmail.com>
2016-02-11 22:46:31 +0100

    remove redundant headers in querycontroller

--- app/App/Http/Controllers/QueryController.php
+++ app/App/Http/Controllers/QueryController.php
@@ -49,10 +49,6 @@ class QueryController extends Controller {
 		}
 
 		$timeFrame = $query->getTimeFrame();
-		header('Access-Control-Allow-Origin: *');
-		header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-		header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-		header('Access-Control-Allow-Credentials: true');
 		return response([
 			'timeframe' => $timeFrame,
 			'data' => $data,
